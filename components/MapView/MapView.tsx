a5873fbb0a7845ea90a00a270cc326fc46d8f43e
Pavel <p4sh0k@gmail.com>
2022-05-23 10:35:30 +0300

    подправил лейаут карты

--- components/MapView/MapView.tsx
+++ components/MapView/MapView.tsx
@@ -6,7 +6,7 @@ import styles from './MapView.module.scss'
 const MapView = () => {
     return (
         <>
-            <YMaps className={styles.mapview}>
+            <YMaps>
                 <Map className={styles.map} defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
             </YMaps>
         </>
